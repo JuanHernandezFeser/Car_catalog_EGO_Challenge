61292644f65956bb0ba3e05fd7c380f15265eb8d
Juan Ignacio Hernández Feser <juani.hernandez.feser.96@gmail.com>
2024-03-19 16:32:59 -0300

    descripcion agregada en modelo

--- api/models.py
+++ api/models.py
@@ -14,6 +14,7 @@ class Car(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     picture = models.ImageField(upload_to='cars_pictures/', null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
     
     def __str__(self):
         return self.brand + self.model + self.year
