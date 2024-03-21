a78a78b07626b1249501348216a4440819729612
Juan Ignacio Hernández Feser <juani.hernandez.feser.96@gmail.com>
2024-03-21 14:46:15 -0300

    cambios en requerimientos y documentacion

--- ego_catalog/settings.py
+++ ego_catalog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    'coreapi',
 ]
 
 STATIC_URL = '/static/'
@@ -129,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
+}
--- ego_catalog/urls.py
+++ ego_catalog/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('docs/', include_docs_urls(title='Api Documentation'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
